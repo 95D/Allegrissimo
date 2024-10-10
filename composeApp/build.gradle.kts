9f5b12e5db52dd93b7321f1eabc539ad234c5d90
gwangjae-lee <gwangjae.lee@linecorp.com>
2024-10-10 22:43:25 +0900

    Add dependency `audioPlayer` module for `composeApp` module

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -61,6 +61,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(project(":audioPlayer"))
         }
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
