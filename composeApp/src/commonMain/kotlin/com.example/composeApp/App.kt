280b5e57ca91d8a1810a0fa7ca2354b20652fcf7
gwangjae-lee <gwangjae.lee@linecorp.com>
2024-10-10 18:31:21 +0900

    Update app UI for checking basic function

--- composeApp/src/commonMain/kotlin/com.example/composeApp/App.kt
+++ composeApp/src/commonMain/kotlin/com.example/composeApp/App.kt
@@ -10,26 +10,46 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.shared.Greeting
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import allegrissimo.composeapp.generated.resources.Res
 import allegrissimo.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.TextField
+import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var bpm by remember { mutableStateOf(70) }
+        var isPlaying by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Row {
+                Text("BPM")
+                TextField(
+                    value = bpm.toString(),
+                    onValueChange = { bpm = it.toInt() },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    )
+                )
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
+            Row {
+                Button(
+                    onClick = {
+                        isPlaying = !isPlaying
+
+                    }
+                ) {
+                    Text("Play")
+                }
+            }
+            AnimatedVisibility(isPlaying) {
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text("Now playing!")
                 }
             }
         }
