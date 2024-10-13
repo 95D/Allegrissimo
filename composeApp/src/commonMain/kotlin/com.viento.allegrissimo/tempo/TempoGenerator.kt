0fb60d5a8b442cf063ed5ce9e6862ba162e5b3c7
gwangjae-lee <gwangjae.lee@linecorp.com>
2024-10-13 16:57:48 +0900

    Update TempoGenerator

--- composeApp/src/commonMain/kotlin/com.viento.allegrissimo/tempo/TempoGenerator.kt
+++ composeApp/src/commonMain/kotlin/com.viento.allegrissimo/tempo/TempoGenerator.kt
@@ -6,43 +6,42 @@ import com.ditchoom.buffer.wrap
 import kotlin.math.PI
 import kotlin.math.sin
 
-class TempoGenerator {
-    fun generateWaveFile(bpm: Int, tone: Int, beat: Int): Pair<ByteArray, ByteArray> {
-        val sampleRate = 44100 // Samples per second
-        val numChannels = 1 // Mono
-        val sampleSize = 16 // Bits per sample
-
-        val durationForBeat = 60f/bpm * tone
-        val samplesPerBeat = (sampleRate * durationForBeat/60).toInt()
-        val duration = (durationForBeat * beat).toInt() // Seconds
-
-        val totalSamples = samplesPerBeat * duration
-        val bytesPerSample = sampleSize / 8
-        val totalBytes = totalSamples * bytesPerSample
-
-        val data = ByteArray(totalBytes)
-
+class TempoGenerator(private val waveConfiguration: WaveConfiguration = WaveConfiguration()) {
+    private fun generateBeepForm(
+        duration: Float
+    ): ByteArray {
+        val totalBytes = waveConfiguration.getTotalBytes(duration)
+        val dataArray = ByteArray(totalBytes)
+        val beepSampleSize = totalBytes / 4
         // Generate audio data (e.g., a sine wave)
-        for (i in 0 until totalSamples) {
-            val sample =  //(sin(i * 2 * PI * tone / sampleRate) * Short.MAX_VALUE)
-                (sin(i * 2 * PI * 440.0 / sampleRate) * Short.MAX_VALUE)
-                .toInt().toShort().toInt()
-            val index = i * bytesPerSample
-            data[index] = (sample and 0xFF).toByte()
-            data[index + 1] = (sample shr 8).toByte()
+        for (i in 0 until beepSampleSize step 2) {
+            val wave = sin(i * 2 * PI * 440.0 / waveConfiguration.sampleRate)
+            val sample = (wave * Short.MAX_VALUE).toInt()
+            dataArray[i] = (sample and 0xFF).toByte()
+            dataArray[i + 1] = (sample shr  Byte.SIZE_BITS).toByte()
+        }
+        return dataArray
+    }
+
+    fun generateWaveBuffer(bpm: Int, tone: Int, beat: Int): Pair<ByteArray, ByteArray> {
+        val durationForBeat = SECONDS_60.toFloat()/bpm.toFloat() * tone
+        val waveFormArray = generateBeepForm(durationForBeat)
+        val dataArray = ByteArray(waveFormArray.size * beat)
+        val dataBuffer = PlatformBuffer.wrap(dataArray, ByteOrder.LITTLE_ENDIAN)
+        // Generate audio data (e.g., a sine wave)
+        for (i in 0 until beat) {
+            dataBuffer.writeBytes(waveFormArray)
         }
 
-        val header = createWaveHeader(sampleRate, numChannels, sampleSize, totalBytes)
-        return header to data
+        val header = createWaveHeader((durationForBeat * beat))
+        return header to dataArray
     }
 
     private fun createWaveHeader(
-        sampleRate: Int,
-        numChannels: Int,
-        sampleSize: Int,
-        totalBytes: Int
+        duration: Float
     ): ByteArray {
-        val bytesPerSample = sampleSize / 8
+        val totalBytes = waveConfiguration.getTotalBytes(duration)
+        val bytesPerSample = waveConfiguration.sampleSize / Byte.SIZE_BYTES
         val chunkSize = totalBytes + 36 // Header size
         val subChunk2Size = totalBytes
 
@@ -54,13 +53,20 @@ class TempoGenerator {
         header.writeString("fmt ")
         header.writeInt(16) // Subchunk 1 size
         header.writeShort(1.toShort()) // Audio format (PCM)
-        header.writeShort(numChannels.toShort())
-        header.writeInt(sampleRate)
-        header.writeInt(sampleRate * numChannels * bytesPerSample)
-        header.writeShort((numChannels * bytesPerSample).toShort())
-        header.writeShort(sampleSize.toShort())
+        header.writeShort(waveConfiguration.numChannels.toShort())
+        header.writeInt(waveConfiguration.sampleRate)
+        val byteRate =
+            waveConfiguration.sampleRate * waveConfiguration.numChannels * bytesPerSample
+        header.writeInt(byteRate)
+        val blockAlign = waveConfiguration.numChannels * bytesPerSample /  Byte.SIZE_BITS
+        header.writeShort(blockAlign.toShort())
+        header.writeShort(waveConfiguration.sampleSize.toShort())
         header.writeString("data")
         header.writeInt(subChunk2Size)
         return byteArray
+    }
+
+    companion object {
+        private const val SECONDS_60 = 60
     }
 }
