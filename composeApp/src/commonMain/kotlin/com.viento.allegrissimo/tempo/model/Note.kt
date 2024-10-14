4bb5b4690b0aac720ea88b7a61ae5a04efc6b15b
gwangjae-lee <gwangjae.lee@linecorp.com>
2024-10-14 18:49:47 +0900

    Create note model

---
+++ composeApp/src/commonMain/kotlin/com.viento.allegrissimo/tempo/model/Note.kt
@@ -0,0 +1,32 @@
+package com.viento.allegrissimo.tempo.model
+
+enum class Note(
+    val divided: Int
+) {
+    THIRTY_SECOND(divided = 32),
+    SIXTEENTH(divided = 16),
+    EIGHTH(divided = 8),
+    QUARTER(divided = 4),
+    HALF(divided = 2),
+    WHOLE(divided = 1);
+
+    val length: Float get() = 4f / divided
+
+    fun up(): Note = when (this) {
+        THIRTY_SECOND -> SIXTEENTH
+        SIXTEENTH -> EIGHTH
+        EIGHTH -> QUARTER
+        QUARTER -> HALF
+        HALF -> WHOLE
+        WHOLE -> WHOLE
+    }
+
+    fun down(): Note = when (this) {
+        THIRTY_SECOND -> THIRTY_SECOND
+        SIXTEENTH -> THIRTY_SECOND
+        EIGHTH -> SIXTEENTH
+        QUARTER -> EIGHTH
+        HALF -> QUARTER
+        WHOLE -> HALF
+    }
+}
