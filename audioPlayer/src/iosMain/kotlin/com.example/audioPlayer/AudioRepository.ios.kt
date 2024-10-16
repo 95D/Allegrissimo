1bc8bb42b68183c9a525be6fbb5fd683eb66bedd
gwangjae-lee <gwangjae.lee@linecorp.com>
2024-10-16 12:21:36 +0900

    Implement AudioRepository for ios platform

--- audioPlayer/src/iosMain/kotlin/com.example/audioPlayer/AudioRepository.ios.kt
+++ audioPlayer/src/iosMain/kotlin/com.example/audioPlayer/AudioRepository.ios.kt
@@ -1,11 +1,44 @@
 package com.example.audioPlayer
 
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.allocArrayOf
+import kotlinx.cinterop.memScoped
+import platform.Foundation.NSData
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSSearchPathForDirectoriesInDomains
+import platform.Foundation.NSString
+import platform.Foundation.NSUserDomainMask
+import platform.Foundation.create
+import platform.Foundation.stringByAppendingPathComponent
+import platform.Foundation.writeToFile
+
 actual class AudioRepository {
-    actual fun saveAudioFile(fileName: String, bytes: ByteArray) {
-        TODO("Not yet implemented")
+    private fun getAudioFilePath(fileName: String): String {
+        val documentDirectory = NSSearchPathForDirectoriesInDomains(
+            NSDocumentDirectory,
+            NSUserDomainMask,
+            true
+        )[0] as NSString
+
+        // Append the file name to the document directory path
+        val filePath = documentDirectory.stringByAppendingPathComponent(fileName)
+        return filePath
     }
 
-    actual fun getFileUri(fileName: String): String {
-        TODO("Not yet implemented")
+    @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+    actual fun saveAudioFile(fileName: String, bytes: ByteArray) {
+        val audioFilePath = getAudioFilePath(fileName)
+        memScoped {
+            NSData.create(bytes = allocArrayOf(bytes), length = bytes.size.toULong())
+                .writeToFile(path = audioFilePath, atomically = true)
+        }
+    }
+
+    actual fun getFileUri(fileName: String): String =
+        getAudioFilePath(fileName)
+
+    companion object {
+        private const val DIR_AUDIO = "Audio"
     }
 }
